b196cddd031019eff1ea6688fc584fbab5b99797
Sriganash <106644052+Sriganash@users.noreply.github.com>
2022-08-02 09:07:36 +0530

    Add files via upload

--- network/1/6/server.c
+++ network/1/6/server.c
@@ -1,85 +1,151 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<pthread.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#define port 8080
 
-pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
+int addrlen;
+int sockfd;
+struct sockaddr_in server, client;
 
-	int sockfd;
-int i;
-int clientfd[5];
+static int k = 0;
+pthread_t t1;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-int chopstick[5];
-int n;
-void wait(int s)
+struct phillosopher
 {
-	while(chopstick[s]<=0);
-	chopstick[s]=0;
+    pthread_t tp;
+    int clientfd;
+    int id;
+    int leftflag, rightflag;
+    int eat;
+    int think;
+    int remtime;
+} * p;
+struct chopstick
+{
+    int id;
+    int flag;
+} * c;
+
+char msg[100];
+
+
+void *allocate(void *arg)
+{
+
+    struct phillosopher *ph = (struct phillosopher *)arg;
+
+    char eat[10] = "EATING";
+    char think[10] = "THINKING";
+    char wait[10] = "WAITING";
+
+    while (ph->remtime > 0)
+    {
+        if (!c[ph->id].flag && !c[((ph->id) + 1) % 5].flag)
+        {
+            pthread_mutex_lock(&lock);
+            c[ph->id].flag = 1;             // grab left
+            c[((ph->id) + 1) % 5].flag = 1; // grab right
+            pthread_mutex_unlock(&lock);
+
+            printf("EATING: %d, REM-TIME %d\n", ph->id, ph->remtime);
+            send(ph->clientfd, eat, sizeof(char) * 10, 0);
+
+            ph->eat = 1; // eat
+            ph->think = 0;
+
+            sleep(5);
+
+            ph->remtime -= 5;
+            c[ph->id].flag = 0;             // release left
+            c[((ph->id) + 1) % 5].flag = 0; // release right
+
+            ph->eat = 0;
+            ph->think = 1; // think
+
+            pthread_mutex_unlock(&lock);
+            sleep(1);
+        }
+        else
+        {
+            printf("THINK: %d\n", p->id);
+            send(ph->clientfd, think, sizeof(char) * 10, 0);
+            sleep(1);
+        }
+    }
+
+    if (ph->remtime == 0)
+    {
+        printf("COMPLETED: %d\n",ph->id);
+        send(ph->clientfd, "COMPLETED", sizeof(char) * strlen("COMPLETED"), 0);
+        pthread_cancel(ph->tp);
+    }
 }
 
-void signal(int s)
+void *acceptConn()
 {
-	chopstick[s]++;
-}
-void *phil(void *p)
-{
-	int i=*(int *)p;
-	printf("\n%d",i);
-	printf("\nphilosopher %d thinking ..........",i+1);
-	wait(i);
-	wait((i+1)%n);
-	sleep(2);
-	printf("\nphilosopher %d finished eating ..........",i+1);
-	signal(i);
-	signal((i+1)%n);
-	
-}
-void *listenAccept(void * arg)
-{
-	while(1)
-	{
-		clientfd[i]=accept(sockfd,(struct sockaddr*)NULL,(socklen_t *)NULL);
+    for (int i = 0; i < 5; i++)
+    {
+        p[i].clientfd = accept(sockfd, (struct sockaddr *)NULL, (socklen_t *)NULL);
+        printf("Phillosopher %d accepted\n", p[i].id);
+    }
 
-		pthread_t t;
-		pthread_create(&t,NULL,phil,(void *)&clientfd[i]);
-
-		pthread_join(t,NULL);
-		pthread_mutex_lock(&lock);
-		i++;
-		pthread_mutex_unlock(&lock);
-	}
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_create(&p[i].tp, NULL, allocate, &p[i]);
+    }
 }
 
 void main()
 {
-	struct sockaddr_in server,client;
 
+    c = (struct chopstick *)malloc(sizeof(struct chopstick) * 5);
+    for (int i = 0; i < 5; i++)
+    {
+        c[i].id = i;
+        c[i].flag = 0;
+    }
 
-	n=5;
-	for(int i=0;i<n;i++)
-		chopstick[i]=1;
-	i=0;
-	int opt;
-	int c=sizeof(struct sockaddr);
+    p = (struct phillosopher *)malloc(sizeof(struct phillosopher) * 5);
+    for (int i = 0; i < 5; i++)
+    {
+        p[i].id = i;
+        p[i].leftflag = 0;  // not allocated the left fork
+        p[i].rightflag = 0; // not allocated the right fork
+        p[i].eat = 0;
+        p[i].think = 1;
+        p[i].remtime = 10;
+    }
 
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Socket creation failed\n");
+    }
+    printf("Socket created successfully\n");
 
-	setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,&opt,sizeof(opt));
+    addrlen = sizeof(server);
 
-	server.sin_family=AF_INET;
-	server.sin_addr.s_addr=INADDR_ANY;
-	server.sin_port=htons(8080);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
 
-	bind(sockfd,(struct sockaddr*)&server,sizeof(server));
+    if (bind(sockfd, (struct sockaddr *)&server, (socklen_t)addrlen) < 0)
+    {
+        perror("Bind failed\n");
+    }
+    printf("Bind successful\n");
 
-	listen(sockfd,5);
+    listen(sockfd, 10);
 
-	pthread_t t1;
-	pthread_create(&t1,NULL,listenAccept,NULL);
-	pthread_join(t1,NULL);
+    pthread_create(&t1, NULL, acceptConn, NULL);
 
+    pthread_join(t1, NULL);
+
+    for (int i = 0; i < 5; i++)
+        pthread_join(p[i].tp, NULL);
+    close(sockfd);
 }
